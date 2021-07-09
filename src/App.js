652ab4ee782fb38425b4a6e55bf543fde6925f6a
Artem Kazaryan <kazarynarm@mail.ru>
2021-07-09 09:56:55 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import img from "./img.jpg";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React App</h1>
+      <img src={img} width="250" />
+    </div>
+  );
+};
+
+export default App;
